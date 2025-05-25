fcb1ac5d770ab230c6330ce098c86aa9692874a9
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-05-25 16:24:53 +0100

    nixos home manager update 25.05

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -13,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
 
   imports = [
     ./programs/dconf/dconf.nix
@@ -68,7 +68,7 @@
     mpv
     ncdu
     ncmpc
-    fira-code-nerdfont
+    nerd-fonts.fira-code
     noto-fonts-emoji
     oath-toolkit
     obs-studio
