82af706eb6f3e37917db84d2c0f085fc0d201f69
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-08-14 12:13:46 +0100

    treesitter

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -54,9 +54,6 @@
         # Add other grammars as needed
       ]))
     ]);
-  extraConfig = ''
-    (setq treesit-extra-load-path '("${pkgs.emacsPackages.tree-sitter.out}/lib/tree-sitter"))
-  '';
   };
 
   # --- OBS Studio Configuration for wlrobs ---
