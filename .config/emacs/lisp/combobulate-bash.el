122f6f78d166bca853d9a5b8d369236b3b2e7bdf
Daniel J Wilcox <danieljwilcox@gmail.com>
2025-09-06 19:33:00 +0100

    another fix

--- .config/emacs/lisp/combobulate-bash.el
+++ .config/emacs/lisp/combobulate-bash.el
@@ -77,7 +77,7 @@
          ;; Add this rule to enable sibling navigation in `case` statements
          (:activation-nodes
           ((:nodes ((rule "case_item") (rule "word"))))
-          :selector (:choose parent :match-children t)))
+          :selector (:choose node :match-siblings t)))
        )
 
       ;; Hierarchical navigation, for moving up and down the syntax tree.
