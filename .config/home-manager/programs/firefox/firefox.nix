c084790b3bab04ced3f6542745bfd5eb85d9cd26
Daniel J Wilcox <danieljwilcox@gmail.com>
2026-03-26 19:39:00 +0000

    firefox remove extrapolicies

--- .config/home-manager/programs/firefox/firefox.nix
+++ .config/home-manager/programs/firefox/firefox.nix
@@ -5,16 +5,6 @@
   programs.firefox = {
     enable = true;
     package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-    extraPolicies = {
-      DisableFirefoxStudies = true;
-      DisablePocket = true;
-      DisableTelemetry = true;
-      DisableFirefoxAccounts = false;
-      NoDefaultBookmarks = true;
-      OfferToSaveLogins = false;
-      OffertosaveloginsDefault = false;
-      PasswordManagerEnabled = false;
-      };
     };
       profiles = {
         default = {
